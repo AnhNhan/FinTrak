d3ca2f9bf0cd6be5fd70bfe928a0785796df60f3
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-08-03 20:09:20 +0200

    Initial commit (new project)

---
+++ FinTrak_WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FinTrak_WP.Resources;
+
+namespace FinTrak_WP
+{
+    /// <summary>
+    /// Bietet Zugriff auf Zeichenfolgenressourcen.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}

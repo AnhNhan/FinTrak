9bab7bdfd19c15ef07772185c20636f7b267562b
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-08-05 05:23:46 +0200

    Prettifying stuff... or not

--- FinTrak_WP/MainPage.xaml.cs
+++ FinTrak_WP/MainPage.xaml.cs
@@ -10,51 +10,60 @@ using Microsoft.Phone.Shell;
 using FinTrak_WP.Resources;
 
 using FinTrak.Asset;
+using FinTrak.Budget;
+using FinTrak.Subject;
+using FinTrak.Transaction;
 
 namespace FinTrak_WP
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private List<ApplicationBarIconButton> AppBarButtons;
+
+        public static AssetCollection Assets { get; private set; }
+        public static TransactionCollection Transactions { get; private set; }
+
         // Konstruktor
         public MainPage()
         {
             InitializeComponent();
+            AppBarButtons = new List<ApplicationBarIconButton>();
+            Assets = new AssetCollection();
+            Transactions = new TransactionCollection();
             AddTestData();
         }
 
         void AddTestData()
         {
-            AssetCollection assetCollection = new AssetCollection();
-
-            assetCollection.Add(new AssetModel
+            Assets.Add(new AssetModel
             {
                 Title = "Dud",
                 TypeId = AssetType.Cash,
                 CurrentBalance = 24.66f,
             });
 
-            assetCollection.Add(new AssetModel
+            Assets.Add(new AssetModel
             {
                 Title = "Dede",
                 TypeId = AssetType.Account,
                 CurrentBalance = -4.37f,
             });
 
-            assetCollection.Add(new AssetModel
+            Assets.Add(new AssetModel
             {
                 Title = "Ha ho",
                 TypeId = AssetType.CashSavings,
                 CurrentBalance = 224.98f,
             });
 
-            assetCollection.Add(new AssetModel
+            Assets.Add(new AssetModel
             {
                 Title = "Dörd",
                 TypeId = AssetType.CashSavings,
                 CurrentBalance = 25.46f,
             });
 
-            assetCollection.Add(new AssetModel
+            Assets.Add(new AssetModel
             {
                 Title = "Hsd",
                 TypeId = AssetType.Prepaid,
@@ -62,7 +71,7 @@ namespace FinTrak_WP
             });
 
             var assetView = new View.AssetsView();
-            assetView.DataContext = assetCollection;
+            assetView.DataContext = Assets;
             // uiRoot_pivot_assets_content.Content = assetView;
             uiRoot_pivot_assets.Content = assetView;
         }
@@ -72,5 +81,45 @@ namespace FinTrak_WP
         private void save_Click(object sender, EventArgs e) { }
 
         private void clear_Click(object sender, EventArgs e) { }
+
+        private void AddAsset_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/View/AssetEditPanel.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void uiRoot_pivot_LoadedPivotItem(object sender, PivotItemEventArgs e)
+        {
+            if (e.Item == uiRoot_pivot_assets)
+            {
+                var button = (ApplicationBarIconButton)Resources["assetsAppBarButton"];
+                AppBarButtons.Add(button);
+                ApplicationBar.Buttons.Add(button);
+
+                if (ApplicationBar.Buttons.Count != 0)
+                {
+                    ApplicationBar.IsVisible = true;
+                }
+            }
+        }
+
+        private void uiRoot_pivot_UnloadingPivotItem(object sender, PivotItemEventArgs e)
+        {
+            List<ApplicationBarIconButton> buttonsForDeletion = new List<ApplicationBarIconButton>();
+            foreach (var button in AppBarButtons)
+            {
+                ApplicationBar.Buttons.Remove(button);
+                buttonsForDeletion.Add(button);
+            }
+
+            foreach (var button in buttonsForDeletion)
+            {
+                AppBarButtons.Remove(button);
+            }
+
+            if (ApplicationBar.Buttons.Count == 0)
+            {
+                ApplicationBar.IsVisible = false;
+            }
+        }
     }
 }

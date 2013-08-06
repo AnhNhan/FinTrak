3a7a5fe07909b97a93de8e679f5781095ad93942
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-08-06 03:28:28 +0200

    Adding SubjectCollection

---
+++ FinTrak/Subject/SubjectCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FinTrak.Asset;
+using FinTrak.Transaction;
+
+namespace FinTrak.Subject
+{
+    public class SubjectCollection : ObservableCollection<SubjectModel>
+    {
+        public SubjectCollection() : base() { }
+        public SubjectCollection(List<SubjectModel> subjects) : base(subjects) { }
+    }
+}

df923d592df9fa6060f82802395294fd6c3bed75
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-08-10 19:35:44 +0200

    Adding BudgetCollection.cs

---
+++ FinTrak/Budget/BudgetCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrak.Budget
+{
+    class BudgetCollection : ObservableCollection<BudgetModel>
+    {
+        public BudgetCollection() : base() { }
+        public BudgetCollection(List<BudgetModel> budgets) : base(budgets) { }
+    }
+}

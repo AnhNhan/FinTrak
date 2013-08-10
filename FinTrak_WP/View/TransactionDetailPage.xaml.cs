b13eaaa8cd7db082967c51c0b9ac9552946c34c1
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-08-10 20:23:01 +0200

    Adding TransactionDetailPage

---
+++ FinTrak_WP/View/TransactionDetailPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using FinTrak.Transaction;
+
+namespace FinTrak_WP.View
+{
+    public partial class TransactionDetailPage : PhoneApplicationPage
+    {
+        public TransactionModel Transaction { get; set; }
+
+        public TransactionDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (NavigationContext.QueryString.ContainsKey("transactionId"))
+            {
+                uint assetId = uint.Parse(NavigationContext.QueryString["transactionId"]);
+                Transaction = MainPage.Transactions.Where(_transaction => _transaction.Id == assetId).First();
+            }
+
+            if (Transaction == null)
+            {
+                MessageBox.Show("The transaction you asked for sadly had not been found. Something went terribly wrong", "Serious trouble :(", MessageBoxButton.OK);
+                NavigationService.GoBack();
+            }
+
+            DataContext = Transaction;
+        }
+    }
+}
--- FinTrak_WP/View/TransactionsView.xaml.cs
+++ FinTrak_WP/View/TransactionsView.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
+using FinTrak.Transaction;
+
 namespace FinTrak_WP.View
 {
     public partial class TransactionsView : UserControl
@@ -15,6 +17,11 @@ namespace FinTrak_WP.View
         public TransactionsView()
         {
             InitializeComponent();
+        }
+
+        private void TransactionView_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MainPage.Navigator.Navigate(new Uri("/View/TransactionDetailPage.xaml?transactionId=" + ((TransactionModel)((TransactionView)sender).DataContext).Id, UriKind.RelativeOrAbsolute));
         }
     }
 }
